4fa591a0881c94a8026cc86d44563ba5a1ed419b
kistikwon <111107076+kistikwon@users.noreply.github.com>
2023-03-27 08:50:23 +0000

    14

--- 03 - TensorFlow Basic/distcifar.py
+++ 03 - TensorFlow Basic/distcifar.py
@@ -1,45 +1,129 @@
-# X 와 Y 의 상관관계를 분석하는 기초적인 선형 회귀 모델을 만들고 실행해봅니다.
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import json
+import math
+import os
+import sys
+import tempfile
+import time
+import numpy as np
+
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
-x_data = [1, 2, 3]
-y_data = [1, 2, 3]
+from tensorflow.keras.datasets.cifar10 import load_data
 
-W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
-# name: 나중에 텐서보드등으로 값의 변화를 추적하거나 살펴보기 쉽게 하기 위해 이름을 붙여줍니다.
-X = tf.placeholder(tf.float32, name="X")
-Y = tf.placeholder(tf.float32, name="Y")
-print(X)
-print(Y)
+flags = tf.app.flags
+flags.DEFINE_string("data_dir", "/tmp/mnist-data",
+                    "Directory for storing mnist data")
+flags.DEFINE_boolean("download_only", False,
+                     "Only perform downloading of data; Do not proceed to "
+                     "session preparation, model definition or training")
+flags.DEFINE_integer("task_index", None,
+                     "Worker task index, should be >= 0. task_index=0 is "
+                     "the master worker task the performs the variable "
+                     "initialization ")
+flags.DEFINE_integer("num_gpus", 1, "Total number of gpus for each machine."
+                     "If you don't use GPU, please set it to '0'")
+flags.DEFINE_integer("replicas_to_aggregate", None,
+                     "Number of replicas to aggregate before parameter update"
+                     "is applied (For sync_replicas mode only; default: "
+                     "num_workers)")
+flags.DEFINE_integer("hidden_units", 100,
+                     "Number of units in the hidden layer of the NN")
+flags.DEFINE_integer("train_steps", 2000,
+                     "Number of (global) training steps to perform")
+flags.DEFINE_integer("batch_size", 600, "Training batch size")
+#flags.DEFINE_integer("batch_size", 100, "Training batch size")
+flags.DEFINE_float("learning_rate", 0.01, "Learning rate")
+flags.DEFINE_boolean(
+    "sync_replicas", False,
+    "Use the sync_replicas (synchronized replicas) mode, "
+    "wherein the parameter updates from workers are aggregated "
+    "before applied to avoid stale gradients")
+flags.DEFINE_boolean(
+    "existing_servers", False, "Whether servers already exists. If True, "
+    "will use the worker hosts via their GRPC URLs (one client process "
+    "per worker host). Otherwise, will create an in-process TensorFlow "
+    "server.")
+flags.DEFINE_string("ps_hosts", "localhost:2222",
+                    "Comma-separated list of hostname:port pairs")
+flags.DEFINE_string("worker_hosts", "localhost:2223,localhost:2224",
+                    "Comma-separated list of hostname:port pairs")
+flags.DEFINE_string("job_name", None, "job name: worker or ps")
 
-# X 와 Y 의 상관 관계를 분석하기 위한 가설 수식을 작성합니다.
-# y = W * x + b
-# W 와 X 가 행렬이 아니므로 tf.matmul 이 아니라 기본 곱셈 기호를 사용했습니다.
-hypothesis = W * X + b
+FLAGS = flags.FLAGS
 
-# 손실 함수를 작성합니다.
-# mean(h - Y)^2 : 예측값과 실제값의 거리를 비용(손실) 함수로 정합니다.
-cost = tf.reduce_mean(tf.square(hypothesis - Y))
-# 텐서플로우에 기본적으로 포함되어 있는 함수를 이용해 경사 하강법 최적화를 수행합니다.
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-# 비용을 최소화 하는 것이 최종 목표
-train_op = optimizer.minimize(cost)
+IMAGE_PIXELS = 28
 
-# 세션을 생성하고 초기화합니다.
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
 
-    # 최적화를 100번 수행합니다.
-    for step in range(100):
-        # sess.run 을 통해 train_op 와 cost 그래프를 계산합니다.
-        # 이 때, 가설 수식에 넣어야 할 실제값을 feed_dict 을 통해 전달합니다.
-        _, cost_val = sess.run([train_op, cost], feed_dict={X: x_data, Y: y_data})
+def next_batch(num, data, labels):
+    idx = np.arange(0 , len(data))
+    np.random.shuffle(idx)
+    idx = idx[:num]
+    data_shuffle = [data[ i] for i in idx]
+    labels_shuffle = [labels[ i] for i in idx]
+    return np.asarray(data_shuffle), np.asarray(labels_shuffle)
 
-        print(step, cost_val, sess.run(W), sess.run(b))
+def build_CNN_classifier(x):
+  keep_prob=0.8
+  x_image = x
+  W_conv1 = tf.Variable(tf.truncated_normal(shape=[5, 5, 3, 64], stddev=5e-2)) # conv2 
+  b_conv1 = tf.Variable(tf.constant(0.1, shape=[64]))
+  h_conv1 = tf.nn.relu(tf.nn.conv2d(x_image, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
+  h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+  W_conv2 = tf.Variable(tf.truncated_normal(shape=[5, 5, 64, 64], stddev=5e-2)) # conv2 
+  b_conv2 = tf.Variable(tf.constant(0.1, shape=[64]))
+  h_conv2 = tf.nn.relu(tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
+  h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+  W_conv3 = tf.Variable(tf.truncated_normal(shape=[3, 3, 64, 128], stddev=5e-2)) # conv2 
+  b_conv3 = tf.Variable(tf.constant(0.1, shape=[128]))
+  h_conv3 = tf.nn.relu(tf.nn.conv2d(h_pool2, W_conv3, strides=[1, 1, 1, 1], padding='SAME') + b_conv3)
+  W_conv4 = tf.Variable(tf.truncated_normal(shape=[3, 3, 128, 128], stddev=5e-2)) # conv2 
+  b_conv4 = tf.Variable(tf.constant(0.1, shape=[128])) 
+  h_conv4 = tf.nn.relu(tf.nn.conv2d(h_conv3, W_conv4, strides=[1, 1, 1, 1], padding='SAME') + b_conv4)
+  W_conv5 = tf.Variable(tf.truncated_normal(shape=[3, 3, 128, 128], stddev=5e-2)) # conv2 
+  b_conv5 = tf.Variable(tf.constant(0.1, shape=[128]))
+  h_conv5 = tf.nn.relu(tf.nn.conv2d(h_conv4, W_conv5, strides=[1, 1, 1, 1], padding='SAME') + b_conv5)
+  W_fc1 = tf.Variable(tf.truncated_normal(shape=[8 * 8 * 128, 384], stddev=5e-2))
+  b_fc1 = tf.Variable(tf.constant(0.1, shape=[384]))
+  h_conv5_flat = tf.reshape(h_conv5, [-1, 8*8*128])
+  h_fc1 = tf.nn.relu(tf.matmul(h_conv5_flat, W_fc1) + b_fc1)
+  h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob) 
+  W_fc2 = tf.Variable(tf.truncated_normal(shape=[384, 10], stddev=5e-2))
+  b_fc2 = tf.Variable(tf.constant(0.1, shape=[10]))
+  logits = tf.matmul(h_fc1_drop,W_fc2) + b_fc2
+  y_pred = tf.nn.softmax(logits)
+  return y_pred, logits
 
-    # 최적화가 완료된 모델에 테스트 값을 넣고 결과가 잘 나오는지 확인해봅니다.
-    print("\n=== Test ===")
-    print("X: 5, Y:", sess.run(hypothesis, feed_dict={X: 5}))
-    print("X: 2.5, Y:", sess.run(hypothesis, feed_dict={X: 2.5}))
+def main(unused_argv):
+  # Parse environment variable TF_CONFIG to get job_name and task_index
+
+  # If not explicitly specified in the constructor and the TF_CONFIG
+  # environment variable is present, load cluster_spec from TF_CONFIG.
+  tf_config = json.loads(os.environ.get('TF_CONFIG') or '{}')
+  task_config = tf_config.get('task', {})
+  task_type = task_config.get('type')
+  task_index = task_config.get('index')
+
+  FLAGS.job_name = task_type
+  FLAGS.task_index = task_index
+  batch_size = FLAGS.batch_size
+
+  x_data = [1, 2, 3]
+  y_data = [1, 2, 3]
+
+  W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+  b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+
+  # name: 나중에 텐서보드등으로 값의 변화를 추적하거나 살펴보기 쉽게 하기 위해 이름을 붙여줍니다.
+  X = tf.placeholder(tf.float32, name="X")
+  Y = tf.placeholder(tf.float32, name="Y")
+  print(X)
+  print(Y)
+
+if __name__ == "__main__":
+  tf.app.run()
